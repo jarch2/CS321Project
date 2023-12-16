1e9afc1a49c75482aa6f0fa960e524fb18037bf8
Joe Arch <jra66@duke.edu>
2023-12-16 16:30:35 -0500

    twosies

--- main.py
+++ main.py
@@ -14,31 +14,57 @@ import networkx as nx
 from GraphRicciCurvature.FormanRicci import FormanRicci
 from GraphRicciCurvature.OllivierRicci import OllivierRicci
 
+import matplotlib.pyplot as plt
+
+# utils
+
+def get_orc_graph(edges, edgefiltration=False, print_flag=False):
+    G = nx.DiGraph(edges)
+    orc_directed = OllivierRicci(G)
+    
+    orc_directed.compute_ricci_curvature()
+
+    if print_flag:
+        for n1, n2 in G.edges():
+            if n1 != n2:
+                print("%d-(%f)>%d" % (n1, orc_directed.G[n1][n2]["ricciCurvature"], n2))
+
+    orc_graph = nx.to_numpy_array(orc_directed.G, weight="ricciCurvature", nonedge=np.inf)
+
+    if edgefiltration:
+        np.fill_diagonal(orc_graph, np.inf)
+
+        for i in range(len(orc_graph)):
+            min = np.inf
+            for j in range(len(orc_graph)):
+                if orc_graph[i][j] < min:
+                    min = orc_graph[i][j]
+                if orc_graph[j][i] < min:
+                    min = orc_graph[j][i]
+            orc_graph[i][i] = min
+    else:
+        np.fill_diagonal(orc_graph, np.min(orc_graph))
+
+    return orc_graph
+
+# premade special graphs
+source_tree = [[1,0]]
+
+############################################
+# execution down here
+############################################
+
 # get graph(s)
 
 graph_info = search(name_or_id = 906)[0]
 graph_path = graph_info.download(extract = True)[0] + "/" + graph_info.name + ".mtx"
 
-print(graph_path)
+graph_path = "./through_flow.mtx"
 y = mmread(graph_path)
 
-# make curvature graphs
+orc_graph = get_orc_graph(y, print_flag=True)
 
-G = nx.DiGraph(y)
-print(G)
-
-orc_directed = OllivierRicci(G)
-orc_directed.compute_ricci_curvature()
-
-print("no")
-
-for n1, n2 in G.edges():
-    print("Directed Graph: The Ollivier-Ricci curvature of edge(%d,%d) id %f" %
-          (n1, n2, orc_directed.G[n1][n2]["ricciCurvature"]))
-
-# plot curvature graphs
-
-X = [y]
+X = [orc_graph]
 
 FP = FlagserPersistence()
 
